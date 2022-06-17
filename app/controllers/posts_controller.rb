1ba527932227ff57a33b24f7d95984d71992c632
achyut kadam <theachyutkadam@gmail.com>
2022-06-17 17:50:54 +0530

    Add CSS to posts on the home page - add CSS to the posts.scss file - add CSS to the mobile.scss to fix too large text issues on smaller screens

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,5 @@
 class PostsController < ApplicationController
   def index
-    puts "+++++++++++++++++++++++333"
-    puts "+++++++++++++++++++++++333"
     @posts = Post.limit(5)
-    puts "+++++++++++++++++++++++222"
-    puts "+++++++++++++++++++++++222"
   end
 end
