bf7d9dde9d6d040832245f44d760d06824f16dbf
achyut kadam <theachyutkadam@gmail.com>
2022-06-17 15:23:56 +0530

    add login feature specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,16 @@
+# FactoryGirl.define do
+#   factory :user do
+#     name { Faker::Name.first_name }
+#     email { Faker::Internet.email }
+#     password { Faker::Internet.password(min_length: 8) }
+#   end
+# end
+
 FactoryGirl.define do
   factory :user do
-    name { Faker::Name.first_name }
-    email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 8) }
+    sequence(:name) { |n| "test#{n}" }
+    sequence(:email) { |n| "test#{n}@test.com" }
+    password '123456'
+    password_confirmation '123456'
   end
+end
